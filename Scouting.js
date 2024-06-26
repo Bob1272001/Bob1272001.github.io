10f98a3119fd350d5e9079a934a56d5052b1aa57
Thomas Jin <83102001+Bob127227311@users.noreply.github.com>
2024-06-26 19:14:02 -0400

    qr code generator

--- Scouting.js
+++ Scouting.js
@@ -5,3 +5,25 @@ function ButtonClicked (){
     const x = 5;
     console.log(x);
 }
+
+function generateQRCode() {
+
+    var Amp = document.getElementById('Amp').value;
+    var AmpMissed = document.getElementById('AmpMissed').value;
+    var Speaker = document.getElementById('Speaker').value;
+    var SpeakerMissed = document.getElementById('SpeakerMissed').value;
+
+    var data = Amp + ', ' + AmpMissed + ', ' + Speaker + ', ' + SpeakerMissed;
+
+    console.log(data);
+
+    var typeNumber = 20;
+    var errorCorrectionLevel = 'L';
+    var qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData(data);
+    qr.make();
+
+    var qrCodeSvg = qr.createSvgTag();
+
+    document.getElementById('qrcode').innerHTML = qrCodeSvg;
+  }

151370fb645dc16b748708ca5a26c7dc71bafcc0
Thomas Jin <83102001+Bob127227311@users.noreply.github.com>
2024-07-05 16:57:13 -0400

    redid html code to look better and made the qr code contain all fields

--- Scouting.js
+++ Scouting.js
@@ -6,24 +6,44 @@ function ButtonClicked (){
     console.log(x);
 }
 
-function generateQRCode() {
+function generateQRCode(formData) {
+  const jsonData = JSON.stringify(formData);
 
-    var Amp = document.getElementById('Amp').value;
-    var AmpMissed = document.getElementById('AmpMissed').value;
-    var Speaker = document.getElementById('Speaker').value;
-    var SpeakerMissed = document.getElementById('SpeakerMissed').value;
+  // Generate QR Code from JSON data
+  var typeNumber = 20;
+  var errorCorrectionLevel = 'L';
+  var qr = qrcode(typeNumber, errorCorrectionLevel);
+  qr.addData(jsonData);
+  qr.make();
 
-    var data = Amp + ', ' + AmpMissed + ', ' + Speaker + ', ' + SpeakerMissed;
+  var qrCodeSvg = qr.createSvgTag();
 
-    console.log(data);
-
-    var typeNumber = 20;
-    var errorCorrectionLevel = 'L';
-    var qr = qrcode(typeNumber, errorCorrectionLevel);
-    qr.addData(data);
-    qr.make();
-
-    var qrCodeSvg = qr.createSvgTag();
-
-    document.getElementById('qrcode').innerHTML = qrCodeSvg;
+  document.getElementById('qrcode').innerHTML = qrCodeSvg;
+}
+  function handleSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    // Do a bit of work to convert the entries to a plain JS object
+    const value = Object.fromEntries(data.entries());
+    
+    console.log(JSON.stringify(value));
   }
+
+  window.addEventListener("load", () => {  
+    const form = document.getElementById('scoutingForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const formData = new FormData(form);
+        const formDataObject = {};
+        
+        formData.forEach((value, key) => {
+            formDataObject[key] = value;
+        });
+
+        generateQRCode(formDataObject);
+    });
+});
+  

f3c64153352893033e734c61b97ba2f9b954163b
Thomas Jin <83102001+Bob127227311@users.noreply.github.com>
2024-10-20 17:05:57 -0400

    yay service worker than I definitely didn't find online and copy and paste in.

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'v1';
+const CACHE_ASSETS = [
+  '/',
+  '/index.html',
+  '/Scouting.css',
+  '/Scouting.js',
+  'https://cdn.jsdelivr.net/npm/qrcode-generator/qrcode.js' // External library
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching files');
+        return cache.addAll(CACHE_ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('Service Worker Activated');
+  // Remove old caches if any
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
